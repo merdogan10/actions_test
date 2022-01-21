0f198ffc2b2ce52029de21e6256f70284dd0a518
merdogan10 <merdogan10@users.noreply.github.com>
2022-01-21 16:59:29 +0000

    style: Automated change

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import re
 
 
-
 import random
 import statistics
 from collections import defaultdict
 import itertools
 import math
+
 sampl_dict = defaultdict(list)
 
 a, b = 3, 5  # sample comment
