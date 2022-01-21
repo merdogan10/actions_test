2e82c1e25d9c94f41096a800d005fe78009c8076
Mustafa <mustafaerdogan1994@gmail.com>
2022-01-21 19:52:04 +0300

    test isort

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import re
-from collections import defaultdict
 
+
+
+import random
+import statistics
+from collections import defaultdict
+import itertools
+import math
 sampl_dict = defaultdict(list)
 
 a, b = 3, 5  # sample comment
