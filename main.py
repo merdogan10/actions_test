fff6e1c63951d67ba4a559a9c4aea23114e6dc8d
Mustafa <mustafaerdogan1994@gmail.com>
2022-01-24 10:14:16 +0300

    fix: test my fixup commit

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-import itertools
-import math
-import random
 import re
+
+
+
+import random
 import statistics
 from collections import defaultdict
-
+import itertools
+import math
 sampl_dict = defaultdict(list)
 
-a, b = 3, 5  # sample comment
+a,b=3,5#sample comment
 
 
-c = a + b + 2
+
+c = a+b + 2
 print("completed")
