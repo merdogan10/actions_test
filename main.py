33e5d664c853ceda5ddc04491ebd14d22e0c3c82
merdogan10 <merdogan10@users.noreply.github.com>
2022-01-24 08:23:04 +0000

    fixup! Update main.py
    Automated change

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-import re
-
-
-
-import random
-import statistics
-from collections import defaultdict       
 import itertools
 import math
+import random
+import re
+import statistics
+from collections import defaultdict
+
 sampl_dict = defaultdict(list)
 
-a,b=3,5#sample comment
+a, b = 3, 5  # sample comment
 
 
-
-c = a+b + 2     
-print("completed")  
+c = a + b + 2
+print("completed")
