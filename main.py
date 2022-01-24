b50ab194f603bc5040e98db9a3fcea6b25ffde16
Mustafa <mustafaerdogan1994@gmail.com>
2022-01-24 11:40:57 +0300

    test pytest

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
+import re
+
+
+
+import random
+import statistics
+from collections import defaultdict       
 import itertools
 import math
-import random
-import re
-import statistics
-from collections import defaultdict
-
 sampl_dict = defaultdict(list)
 
-a, b = 3, 5  # sample comment
+a,b=3,5#sample comment
+
+def add5(var):
 
 
-c = a + b + 2
-print("completed")
+
+    return var+5   
+
+
+
+
+def test_add5():
+    assert add5(4) == 9  
+
+c = a+b + 2     
+print("completed")  
